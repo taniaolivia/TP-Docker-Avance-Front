67a55cf15418b3a0bdadb6ee3c652e764a5eff2c
Coumbadiankha <coumba.diankha@hotmail.fr>
2023-09-06 21:51:47 +0200

    Create elan.js

---
+++ src/Elan.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function ElanImage() {
+  return (
+    <div>
+      <h1>Elan en anglais, ca se dit ELK !</h1>
+      <img
+        src="/images/elk.jpg" // Assurez-vous que le chemin de l'image est correct
+        alt="Élan"
+        width="300" // Ajustez la largeur selon vos besoins
+        height="200" // Ajustez la hauteur selon vos besoins
+      />
+    </div>
+  );
+}
+
+export default ElanImage;
