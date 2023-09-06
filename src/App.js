a13c1ea00167e6cbb1b127c46686384eed798234
Coumbadiankha <coumba.diankha@hotmail.fr>
2023-09-06 21:51:27 +0200

    create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ElanImage from './ElanImage';
+
+function App() {
+  return (
+    <div className="App">
+      <ElanImage />
+    </div>
+  );
+}
+
+export default App;
